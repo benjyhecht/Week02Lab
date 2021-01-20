ac8352f66e6a2c3e8272aa11f99d59c12007784a
468181 <468181@SAIT225790IT.ACDM.DS.SAIT.CA>
2021-01-20 15:54:41 -0700

    New Project Creation for Lab 2

---
+++ src/week02lab2/Week02Lab2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02lab2;
+
+/**
+ *
+ * @author 468181
+ */
+public class Week02Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Lab 2 repository created!");
+    }
+    
+}
